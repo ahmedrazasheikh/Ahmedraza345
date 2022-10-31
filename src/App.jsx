dfe6034a188c7ecc95d20c2d628ffd5f719526d7
Ahmed Raza <bhaia9036@gmail.com>
2022-10-31 00:08:11 -0700

    initail commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Anonymoous</h1>
+<h2>Ahmed Raza</h2>
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam perspiciatis adipisci impedit cupiditate eligendi sit quia molestias, aspernatur incidunt velit quasi et facilis quae soluta, alias nihil officiis officia fugit.</p>
+    </div>
+  );
+}
+
+export default App;
